6dbd3bdf0a20031cea4a8bd83f240f9c432ccc82
Varun <vyelluru@gmail.com>
2023-11-18 20:40:35 -0500

    8 pm update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,35 +3,66 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 import numpy as np
+from hugchat import hugchat
+from hugchat.login import Login
 
 # Set up the Streamlit app title and description
 st.title("Streamlit Page")
 
 # Load example data
-data = pd.DataFrame({
-    'Category': ['A', 'B', 'C', 'D'],
-    'Values': [4, 7, 1, 5]
+df = pd.DataFrame({
+    'x': [1, 2, 3, 4],
+    'y': [4, 7, 1, 5]
 })
 
 # Scatter Plot
 st.header("Scatter Plot")
-fig_scatter = px.scatter(data, x='Category', y='Values', title='Scatter Plot')
+
+selected_product = st.selectbox(label="Select Product:", options=[1, 2, 3])
+selected_start_date = st.date_input("Start Date:")
+selected_end_date = st.date_input("End Date:")
+
+df.index = pd.to_datetime(df.index)
+
+selected_start_date = pd.to_datetime(selected_start_date)
+selected_end_date = pd.to_datetime(selected_end_date)
+
+query = f"index >= '{selected_start_date}' and index <= '{selected_end_date}'"
+df_filtered = df.query(query)
+
+fig_scatter = px.scatter(df_filtered, x='x', y='y', title='Scatter Plot')
 st.plotly_chart(fig_scatter, use_container_width=True)
 
 # Area Chart
 st.header("Area Chart")
-fig_area = px.area(data, x='Category', y='Values', title='Area Chart')
+fig_area = px.area(df, x='x', y='y', title='Area Chart')
 st.plotly_chart(fig_area, use_container_width=True)
 
-
-st.sidebar.header("Chatbot")
 user_input = st.sidebar.text_input("Enter your message:")
-if user_input:
+
+with st.sidebar:
+    st.sidebar.header("Chatbot")
+    if user_input:
     # Add your chatbot logic here
     # You can use an external library or define a function to handle the chatbot responses
     # For simplicity, let's echo back the user's input for now
+        if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
+            st.success('HuggingFace Login credentials already provided!', icon='✅')
+            hf_email = st.secrets['EMAIL']
+            hf_pass = st.secrets['PASS']
+        else:
+            hf_email = st.text_input('Enter E-mail:', type='password')
+            hf_pass = st.text_input('Enter password:', type='password')
+            if not (hf_email and hf_pass):
+                st.warning('Please enter your credentials!', icon='⚠️')
+            else:
+                st.success("Please enter question.")
     st.sidebar.text(f"User: {user_input}")
     st.sidebar.text("Chatbot: " + user_input)
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
 #Upload Files
 st.header("Upload Files")
